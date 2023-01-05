cdaf41b3e017d9d806c2e9dbe8647c62c382d650
Ricardo J <nclass.dev@gmail.com>
2023-01-05 15:15:53 -0600

    Adding Screens and Navigation

---
+++ app/src/main/java/com/phrasenote/PhraseDetailFragment.kt
@@ -0,0 +1,16 @@
+package com.phrasenote
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PhraseDetailFragment : Fragment(R.layout.fragment_phrase_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
